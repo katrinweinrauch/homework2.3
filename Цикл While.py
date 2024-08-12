dcb1f6769acf9dcc9b2364ec02a904cdc1d445c6
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-08-12 22:49:04 +0500

    Add files via upload

---
+++ Цикл While.py
@@ -0,0 +1,49 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+k = (len(my_list))
+i = my_list[0]
+while k <= 12:
+    if my_list[0]> 0:
+        print(my_list[0])
+    if my_list[0] < 0:
+        break
+    if my_list[2] > 0:
+        print(my_list[2])
+    if my_list[2] < 0:
+        break
+    if my_list[3] > 0:
+        print(my_list[3])
+    if my_list[3] < 0:
+        break
+    if my_list[4] > 0:
+        print(my_list[4])
+    if my_list[4] < 0:
+        break
+    if my_list[5] > 0:
+        print(my_list[5])
+    if my_list[5] < 0:
+        break
+    if my_list[6] > 0:
+        print(my_list[6])
+    if my_list[6] < 0:
+        break
+    if my_list[7] > 0:
+        print(my_list[7])
+    if my_list[7] < 0:
+        break
+    if my_list[8] > 0:
+        print(my_list[8])
+    if  my_list[8] < 0:
+        break
+    if my_list[9] > 0:
+        print(my_list[9])
+    if my_list[9] < 0:
+        break
+    if my_list[10] > 0:
+        print(my_list[10])
+    if  my_list[10] < 0:
+        break
+    if my_list[11] > 0:
+        print(my_list[11])
+    if  my_list[11] < 0:
+        break
+
